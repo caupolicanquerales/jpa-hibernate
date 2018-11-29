8e553b7c4df46087c24b70a2eb077ecfb15ff92f
Caupolican <caupolicanquerales@gmail.com>
2018-11-29 18:51:22 -0300

    Se retira comentario porque es irrelevante

--- src/main/java/com/pruebaSpring/jpa/pruebajpa/PruebaJpaApplication.java
+++ src/main/java/com/pruebaSpring/jpa/pruebajpa/PruebaJpaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PruebaJpaApplication {
 
-	// comentario nuevo
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PruebaJpaApplication.class, args);
 	}
